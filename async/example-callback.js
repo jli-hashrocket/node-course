91413ed0365d19b50cd6da406116de66bd0da8d0
macheteshuffle <jeff.li029@gmail.com>
2016-07-04 23:10:23 -0400

    callbacks with weather api exercise

--- async/example-callback.js
+++ async/example-callback.js
@@ -1,17 +1,5 @@
-var request = require('request');
-var encodedLocation = "Washington,dc";
-var url = 'http://api.openweathermap.org/data/2.5/weather?appid=2cc14f1ea516b96a82fbede67ca1f029&q=' + encodedLocation + '&units=imperial';
+var weather = require('./weather.js');
 
-request({
-  url: url,
-  json: true
-}, function(error, response, body){
-  if (error) {
-    console.log('Unable to fetch weather');
-  } else {
-    // console.log(JSON.stringify(body, null, 4));
-    console.log("It's " + body.main.temp + " in " + body.name);
-  }
+weather(function(currentWeather){
+  console.log(currentWeather);
 });
-
-console.log('After request!');
---
+++ async/weather.js
@@ -0,0 +1,20 @@
+var request = require('request');
+var encodedLocation = "Washington,dc";
+var url = 'http://api.openweathermap.org/data/2.5/weather?appid=2cc14f1ea516b96a82fbede67ca1f029&q=' + encodedLocation + '&units=imperial';
+
+module.exports = function (callback) {
+  callback('Here is the current weather!');
+  request({
+    url: url,
+    json: true
+  }, function(error, response, body){
+    if (error) {
+      callback('Unable to fetch weather');
+    } else {
+      // console.log(JSON.stringify(body, null, 4));
+      callback("It's " + body.main.temp + " in " + body.name);
+    }
+  });
+
+
+};
