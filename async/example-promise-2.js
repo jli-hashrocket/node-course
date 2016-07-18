643dda21524dd80e4a85ed21bea8a9f3e7e8430f
macheteshuffle <jeff.li029@gmail.com>
2016-07-17 22:03:53 -0400

    passing data from promise to promise using weather api

--- async/example-promise-2.js
+++ async/example-promise-2.js
@@ -1,20 +1,72 @@
-function doWork (shouldFail) {
-  return new Promise(function (resolve, reject){
-    setTimeout(function () {
-      if (typeof shouldFail === 'boolean' && shouldFail === true){
-        reject('error message');
+// function doWork (shouldFail) {
+//   return new Promise(function (resolve, reject){
+//     setTimeout(function () {
+//       if (typeof shouldFail === 'boolean' && shouldFail === true){
+//         reject('error message');
+//       } else {
+//         resolve('success');
+//       }
+//     }, 1000);
+//   });
+// }
+
+// doWork().then(function(message){
+//   console.log(message);
+//   return doWork(true);
+// }).then(function (message) {
+//   console.log(message);
+// }).catch(function (error) {
+//   console.log(error);
+// });
+var url = 'http://ipinfo.io';
+var request = require('request');
+
+function getLocation(location){
+  //return promise
+    //resolve('washingotn, dc');
+  return new Promise( function(resolve, reject){
+    request({
+      url: url,
+      json: true
+    }, function(error, response, body){
+      if (error) {
+        reject('Unable to get location');
       } else {
-        resolve('success');
+        resolve(location);
       }
-    }, 1000)
+    });
+
   });
 }
 
-doWork().then(function(message){
-  console.log(message);
-  return doWork(true);
-}).then(function (message) {
-  console.log(message);
-}).catch(function (error) {
-  console.log(error);
-})
+function getWeather(location){
+  // return promise
+  //  resolve('It's 78 in location)
+  return new Promise( function(resolve, reject) {
+    var encodedLocation = encodeURIComponent(location);
+
+    if (!location) {
+      return reject('No location provided');
+    }
+
+    var url = 'http://api.openweathermap.org/data/2.5/weather?appid=2cc14f1ea516b96a82fbede67ca1f029&q=' + encodedLocation + '&units=imperial';
+
+    request({
+      url: url,
+      json: true
+    }, function(error, response, body){
+      if (error) {
+        reject('Unable to fetch weather');
+      } else {
+        // console.log(JSON.stringify(body, null, 4));
+        resolve("It's " + body.main.temp + " in " + body.name);
+      }
+    });
+  });
+}
+
+getLocation('washington, dc').then(function(location){
+  return getWeather(location)
+}).then(function(currentWeather){
+  console.log(currentWeather);
+});
